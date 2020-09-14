16d51864e922de6af45d7ae6d2a9d9f116e142ca
Vinicius Soares <vsoares.a@gmail.com>
2020-09-14 14:55:29 -0300

    moficacoes nao concluidas

--- src/test/java/com/vsoares/infosw/InfoswApplicationTests.java
+++ src/test/java/com/vsoares/infosw/InfoswApplicationTests.java
@@ -1,9 +1,11 @@
 package com.vsoares.infosw;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DataMongoTest
 class InfoswApplicationTests {
 
     @Test
